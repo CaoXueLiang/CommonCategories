e821bdf5c35b23e2211a549aca8fc4deca8fc234
曹学亮 <2357747972@qq.com>
2018-12-26 14:47:42 +0800

    备份

---
+++ CommonCategories/Categories/UIImage+fixOrientation.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+fixOrientation.h
+//  MeiLin
+//
+//  Created by 曹学亮 on 2016/11/17.
+//  Copyright © 2016年 Li Chuanliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ 调整图片方向
+ */
+@interface UIImage (fixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
